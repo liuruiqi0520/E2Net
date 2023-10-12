5275c43f5859da53acb163d417e8ed26871bbef5
liuruiqi <1548885166@qq.com>
2023-10-12 16:34:31 +0800

    version2

--- e2n_utils.py
+++ e2n_utils.py
@@ -21,7 +21,6 @@ def get_net(args):
     return backbone
 
 def self_search(t,model,sub_net_dict,last_config,finconfig_list,total_config_dict):
-    CNS = 1
     z = torch.randn((32, 3, 32, 32))
     z = z.to(model.device)
     max_output = model.net(z)
@@ -38,46 +37,34 @@ def self_search(t,model,sub_net_dict,last_config,finconfig_list,total_config_dic
     limit_paramerters = net_paramerters * 0.95
     sub_limit_paramerters = limit_paramerters
     sub_paramerters = dict()
-    if CNS:
-        for i in range(500):
-            sub_paramerters["Total"] = net_paramerters
-            sub_config = model.net.sample_active_subnet2(t=t, config_list=last_config)
-            subnet = model.net.get_active_subnet()
-            sub_paramerters = get_parameter_number(subnet)
-            occu = round(sub_paramerters["Total"] / net_paramerters, 3)
-            sub_output = model.net(z)
-            sub_loss = F.mse_loss(sub_output, max_output) * math.exp(-occu)
-            # sub_loss = F.mse_loss(sub_output, max_output)
-            sub_config_list.append(sub_config)
-            sub_loss_list.append(sub_loss.item())
-
-        sub_loss_list = torch.tensor(sub_loss_list)
-        sorted_importance, sorted_idx = torch.sort(sub_loss_list, dim=0, descending=False)
-        for i in range(30):
-            model.net.set_active_subnet(**sub_config_list[sorted_idx[i]])
-            subnet = model.net.get_active_subnet()
-            sub_paramerters = get_parameter_number(subnet)
-            total_paramerters += sub_paramerters["Total"]
-            if total_paramerters < limit_paramerters and sub_paramerters["Total"] < sub_limit_paramerters:
-                super_net_list.append(subnet)
-                super_parameters_list.append(sub_paramerters["Total"])
-                parameters_occupy.append(round(sub_paramerters["Total"] / net_paramerters, 3))
-                super_loss_list.append(sub_loss_list[sorted_idx[i]].tolist())
-                super_config_list.append(sub_config_list[sorted_idx[i]])
-                if len(super_config_list) == 1: break
-            else:
-                total_paramerters -= sub_paramerters["Total"]
-    else:
-        model.net.set_active_subnet(maxnet=True)
+    for i in range(500):
+        sub_paramerters["Total"] = net_paramerters
+        sub_config = model.net.sample_active_subnet2(t=t, config_list=last_config)
         subnet = model.net.get_active_subnet()
-        sub_output = model.net(z)
-        sub_loss = F.mse_loss(sub_output, max_output)
         sub_paramerters = get_parameter_number(subnet)
-        super_parameters_list = [sub_paramerters["Total"]]
-        super_net_list = [subnet]
-        super_config_list = [model.net.get_sub_config()]
-        super_loss_list = [sub_loss.item()]
-        parameters_occupy = [round(sub_paramerters["Total"] / net_paramerters, 3)]
+        occu = round(sub_paramerters["Total"] / net_paramerters, 3)
+        sub_output = model.net(z)
+        sub_loss = F.mse_loss(sub_output, max_output) * math.exp(-occu)
+        # sub_loss = F.mse_loss(sub_output, max_output)
+        sub_config_list.append(sub_config)
+        sub_loss_list.append(sub_loss.item())
+
+    sub_loss_list = torch.tensor(sub_loss_list)
+    sorted_importance, sorted_idx = torch.sort(sub_loss_list, dim=0, descending=False)
+    for i in range(30):
+        model.net.set_active_subnet(**sub_config_list[sorted_idx[i]])
+        subnet = model.net.get_active_subnet()
+        sub_paramerters = get_parameter_number(subnet)
+        total_paramerters += sub_paramerters["Total"]
+        if total_paramerters < limit_paramerters and sub_paramerters["Total"] < sub_limit_paramerters:
+            super_net_list.append(subnet)
+            super_parameters_list.append(sub_paramerters["Total"])
+            parameters_occupy.append(round(sub_paramerters["Total"] / net_paramerters, 3))
+            super_loss_list.append(sub_loss_list[sorted_idx[i]].tolist())
+            super_config_list.append(sub_config_list[sorted_idx[i]])
+            if len(super_config_list) == 1: break
+        else:
+            total_paramerters -= sub_paramerters["Total"]
     finconfig_list["config"].extend(super_config_list)
     finconfig_list["loss"].extend(super_loss_list)
     last_config = super_config_list
